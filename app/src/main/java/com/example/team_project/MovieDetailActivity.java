7d5ab0043466250e1b9dfb37cd87e7ca7d07a9cf
hyeesw <hyeeun@kookmin.ac.kr>
2022-11-09 21:00:31 +0900

    upload git

---
+++ app/src/main/java/com/example/team_project/MovieDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.team_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MovieDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+    }
+}
