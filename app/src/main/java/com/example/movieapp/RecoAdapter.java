2e7a79f5bcafc055757524f6d2e310ae4c6a48e0
gyuwonsong <gyuwon0722@kookmin.ac.kr>
2022-11-25 06:23:36 +0900

    파이어베이스 연ë 연ã동, csv ã-> json, ì리사이클러 뷰 에러 수정

--- app/src/main/java/com/example/movieapp/RecoAdapter.java
+++ app/src/main/java/com/example/movieapp/RecoAdapter.java
@@ -12,18 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 public class RecoAdapter extends RecyclerView.Adapter<RecoAdapter.CustomViewHolder> {
-
-    //리스트뷰의 아이템들을 담을 리스트
     private ArrayList<MovieItem> movieList;
-    //생성자
+
     public RecoAdapter(ArrayList<MovieItem> mMovieList) {
         this.movieList = mMovieList;
     }
 
     @NonNull
     @Override
-    //리스트뷰가 처음으로 생성될 때 생명주기
-    public RecoAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recyclerview, parent, false);
         RecoAdapter.CustomViewHolder holder = new RecoAdapter.CustomViewHolder(view);
 
@@ -31,19 +28,14 @@ public class RecoAdapter extends RecyclerView.Adapter<RecoAdapter.CustomViewHold
     }
 
     @Override
-    //실제 추가될 때 생명주기
     public void onBindViewHolder(@NonNull RecoAdapter.CustomViewHolder holder, int position) {
-        //프로필 사진 가져오기
         holder.img.setImageResource(movieList.get(position).getImg());
         holder.name.setText(movieList.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        if (movieList!=null){
-            return movieList.size();
-        }
-        else return 0;
+        return movieList.size();
     }
 
     public void setMovieList(ArrayList<MovieItem> mMovieList){
