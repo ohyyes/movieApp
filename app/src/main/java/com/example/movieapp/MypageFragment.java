4cab60edfb8bacf3fbf294994d7c4b52577f8689
judyzero <qozmcll@kookmin.ac.kr>
2022-11-24 20:42:25 +0900

    merge 수정

--- app/src/main/java/com/example/movieapp/MypageFragment.java
+++ app/src/main/java/com/example/movieapp/MypageFragment.java
@@ -102,6 +102,7 @@ public class MypageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         //Firebase 로그인한 사용자 정보
 
         mAuth = FirebaseAuth.getInstance();
@@ -109,23 +110,8 @@ public class MypageFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_mypage, container, false);
 
-
-        ImageButton ib_more = (ImageButton)rootView.findViewById(R.id.ib_more);     //더보기 버튼
-        ImageButton ib_edit_profile = (ImageButton)rootView.findViewById(R.id.ib_edit_profile);     //프로필수정 버튼
-
-        //프로필수정 버튼 눌렀을 때,
-        ib_edit_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Edit_profile.xml 로 이동
-                Intent intent = new Intent(activity.getApplicationContext(), EditProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-
         Thread mThread = new Thread();
         mThread.start();
-
 
         TextView tv_nickname = (TextView) view.findViewById(R.id.tv_nickname);
         tv_nickname.setText(user.getDisplayName());
