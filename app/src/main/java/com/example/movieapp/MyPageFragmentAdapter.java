cfe61d4348bddb9992758465ef32c9855117419a
hyeesw <hyeeun@kookmin.ac.kr>
2022-11-28 01:52:44 +0900

    [feat] MypageFragment 의 리사이클러뷰 아이템 선택시에 감상평 상세 페이지로 화면전환 구현

--- app/src/main/java/com/example/movieapp/MyPageFragmentAdapter.java
+++ app/src/main/java/com/example/movieapp/MyPageFragmentAdapter.java
@@ -1,5 +1,6 @@
 package com.example.movieapp;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +14,23 @@ import java.util.ArrayList;
 
 public class MyPageFragmentAdapter extends RecyclerView.Adapter<MyPageFragmentAdapter.CustomViewHolder> {
 
+    //홈액티비티 가져오기 (선언)
+    HomeActivity homeActivity;
+
     //리스트뷰의 아이템들을 담을 리스트
     private ArrayList<MyPageFragmentMainData> arrayList;
     //생성자
-    public MyPageFragmentAdapter(ArrayList<MyPageFragmentMainData> arrayList) {
+    public MyPageFragmentAdapter(ArrayList<MyPageFragmentMainData> arrayList, Context mContext) {
         this.arrayList = arrayList;
+
+        // 어댑터에서는 다른 엑티비티를 가져올 때 Context 로 가져옴 !
+        this.homeActivity = ((HomeActivity) mContext);
     }
 
     @NonNull
     @Override
-    //리스트뷰가 처음으로 생성될 때 생명주기
+    //리사이클러뷰가 처음으로 생성될 때 생명주기
     public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
         // mypage_recycleview_items.xml 과 연동하기
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.mypage_recyclerview_items, parent, false);
         //holder로 mypage_recycleview_items.xml 의 뷰를 컨택할 수 있음.
@@ -41,14 +47,16 @@ public class MyPageFragmentAdapter extends RecyclerView.Adapter<MyPageFragmentAd
 
         //리스트뷰가 클릭되었을 때,
         holder.itemView.setTag(position);
-//        holder.itemView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String curName = holder.tv_name.getText().toString();
-//                //curName이 토스트 메뉴로 클릭되게
-//                Toast.makeText(view.getContext(), curName, Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /* searchFragmentAdapter.java 참고해서 데이터 가져와서 전달하는 코드 작성 */
+
+                //homeActivity.java 에서 선언된 onFragmentChange 메소드에 접근해서 index=1 을 전달해 fragmentReview 로 이동 (감상평 상세 페이지)
+                homeActivity.onFragmentChange(1);
+            }
+        });
 
     }
 
